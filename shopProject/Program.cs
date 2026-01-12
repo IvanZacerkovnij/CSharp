8f565bc7721dc1757aa65645ef18b21fc05b98e8
IvanZacerkovnij <ivan.zacerkovnij@gmail.com>
2026-01-12 19:08:15 +0200

    some changes

---
+++ shopProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace shopProject;
+
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        Shop kopiechka = new Shop("Kopiechka");
+        
+        kopiechka.AddProduct(new Product("Maria",2.99));
+        kopiechka.AddProduct(new Product("Jerry",2.99));
+        kopiechka.AddProduct(new Product("Lays",6.99));
+
+        foreach (var product in kopiechka)
+        {
+            Console.WriteLine(product);
+        }
+    }
+}
