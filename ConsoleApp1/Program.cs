13482271bc253f56282eaa571c97d1a94c477b4b
IvanZacerkovnij <ivan.zacerkovnij@gmail.com>
2026-01-16 20:47:26 +0200

    update project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1;
+
+class Car
+{
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public Car(string model, int year)
+    {
+        Model = model;
+        Year = year;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car("audi", 2020);
+        int i = 0;
+        while (i < 130_000)
+        {
+            Car car2 = new Car("bmw", 2000);
+            i++;
+        }
+        Console.WriteLine("байт в купі: {0}",
+            GC.GetTotalMemory(false));
+        Console.WriteLine(GC.GetGeneration(car));
+    }
+}
